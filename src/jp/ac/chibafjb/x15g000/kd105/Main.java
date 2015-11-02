d3e389fec7e8e8e8aebc43fc18275d26fc861d22
unknown <oikawa@PC504N000.chiba-fjb.ac.jp>
2015-11-02 14:54:00 +0900

    4つ目と5つ目

---
+++ src/jp/ac/chibafjb/x15g000/kd105/Main.java
@@ -0,0 +1,11 @@
+package jp.ac.chibafjb.x15g000.kd105;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("氏名：　船橋　太郎");
+		System.out.println("趣味：　主熱圏を食べさせる");
+		System.out.println("特技：　ですソースを食べさせる");
+	}
+
+}
